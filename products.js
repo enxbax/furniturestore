40b7a51e17faaf66c7e6d1df895faf06f2c1cbb0
NBA <heartripper1881@gmail.com>
2026-07-22 05:50:06 +0700

    Add files via upload

---
+++ products.js
@@ -0,0 +1,140 @@
+
+  
+  const container = document.getElementById("products");
+  
+  function drawProducts(list) {
+  
+      container.innerHTML = "";
+  
+      list.forEach(product => {
+  
+          container.innerHTML += `
+  
+  <div class="card">
+  
+  <img src="${product.preview}" alt="${product.name}">  
+  <div class="card-body">
+  
+  <h3>${product.name}</h3>
+  
+  <p>${product.description}</p>
+  
+  <div class="price">${product.price}</div>
+  
+    <a class="more" href="product.html?id=${product.id}&cat=${encodeURIComponent(product.category)}&sub=${encodeURIComponent(product.subcategory)}">
+    Подробнее
+    </a>
+  
+  </div>
+  
+  </div>
+  
+  `;
+  
+      });
+  
+  }
+  
+  drawProducts(products);
+  
+  
+function filterProducts(category){
+
+    if(category==="Все"){
+    
+    drawProducts(products);
+    return;
+    
+    }
+    
+    const filtered = products.filter(product=>product.category===category);
+    
+    drawProducts(filtered);
+    
+    }
+    const phoneButton = document.getElementById("copyPhone");
+
+if(phoneButton){
+
+    phoneButton.addEventListener("click", function(){
+
+        const phone = document.getElementById("phoneNumber").innerText;
+
+        if(navigator.clipboard){
+
+            navigator.clipboard.writeText(phone);
+
+        }else{
+
+            const input=document.createElement("input");
+            input.value=phone;
+
+            document.body.appendChild(input);
+
+            input.select();
+
+            document.execCommand("copy");
+
+            input.remove();
+
+        }
+
+        phoneButton.innerHTML="✅ Номер скопирован";
+
+        setTimeout(function(){
+
+            phoneButton.innerHTML="📋 Скопировать номер";
+
+        },2000);
+
+    });
+
+}
+const scrollBtn = document.getElementById("scrollBottom");
+
+if(scrollBtn){
+
+    scrollBtn.addEventListener("click", () => {
+
+        window.scrollTo({
+
+            top: document.body.scrollHeight,
+
+            behavior: "smooth"
+
+        });
+
+    });
+
+}
+const scrollTopBtn = document.getElementById("scrollTop");
+
+if(scrollTopBtn){
+
+    window.addEventListener("scroll", function(){
+
+        if(window.scrollY > 500){
+
+            scrollTopBtn.style.display = "block";
+
+        }else{
+
+            scrollTopBtn.style.display = "none";
+
+        }
+
+    });
+
+    scrollTopBtn.onclick = function(){
+
+        window.scrollTo({
+
+            top:0,
+
+            behavior:"smooth"
+
+        });
+
+    };
+
+}
